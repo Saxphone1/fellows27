bfeba0a39a3709832c778769e82ee211c7a657d5
wuying2015 <wuy_123@126.com>
2018-11-10 10:09:40 +0800

    test

---
+++ 02/src/Test21.java
@@ -0,0 +1,11 @@
+public class Test21 {
+    //
+    public static void main(String[] args) {
+        int i=0;
+        for(;;){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
