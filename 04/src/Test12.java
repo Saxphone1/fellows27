212e9ae2726fd19ede50e479a01c0a00b083e9a4
wuying2015 <wuy_123@126.com>
2018-11-17 14:59:15 +0800

    test

---
+++ 04/src/Test12.java
@@ -0,0 +1,9 @@
+public class Test12 {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for (String s:args) {
+            System.out.println(s);
+        }
+    }
+}
