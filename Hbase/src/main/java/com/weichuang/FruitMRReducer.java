f182dffdf822c6f8bfd0c2d1d43d9b50be549606
Dxl <wuy_123@126.com>
2019-02-20 16:07:54 +0800

    test

---
+++ Hbase/src/main/java/com/weichuang/FruitMRReducer.java
@@ -0,0 +1,20 @@
+package com.weichuang;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FruitMRReducer extends TableReducer<ImmutableBytesWritable, Put, NullWritable>{
+    @Override
+    protected void reduce(ImmutableBytesWritable key, Iterable<Put> values, Context context) throws IOException, InterruptedException {
+        //读出来的每一行数据写入到fruit_mr表中
+        for(Put put: values){
+            context.write(NullWritable.get(), put);
+        }
+
+    }
+}
