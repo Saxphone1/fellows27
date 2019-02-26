e118c4e1e5877e8b380d12a2fbaebd2f08a5dd3b
Dxl <wuy_123@126.com>
2019-02-26 14:50:36 +0800

    test

--- kafka/src/main/java/com/weichuang/NewProducer.java
+++ kafka/src/main/java/com/weichuang/NewProducer.java
@@ -1,8 +1,9 @@
 package com.weichuang;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+import kafka.tools.ConsoleProducer;
+import org.apache.kafka.clients.producer.*;
 
 import java.util.Properties;
 
@@ -10,9 +11,9 @@ public class NewProducer {
     public static void main(String[] args) {
         Properties props = new Properties();
         // 等待所有副本节点的应答
-        props.put(ProducerConfig.ACKS_CONFIG,"all");
+        props.put("acks","all");
         props.put("bootstrap.servers", "hadoop101:9092");
-
+        props.put("partitioner.class","com.weichuang.KafkaPartitioner");
         // key序列化
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         // value序列化
@@ -20,8 +21,17 @@ public class NewProducer {
 
         KafkaProducer producer = new KafkaProducer(props);
         for(int i=0;i<10;i++){
-            ProducerRecord record = new ProducerRecord("first", String.valueOf(i));
-            producer.send(record);
+            ProducerRecord record = new ProducerRecord("second", String.valueOf(i));
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if(exception == null){
+                        System.out.printf(metadata.partition()+"=>>"+metadata.topic()+","+metadata.offset());
+                    }else{
+                        System.out.println("发送失败！！！");
+                    }
+                }
+            });
         }
         producer.close();
 
